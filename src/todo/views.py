1a9070e42fff2cacca6e382ea4aaeb5f77af5adb
KamilCetiner <cetiner.kamil84@gmail.com>
2021-01-11 12:47:32 +0100

    add Todo Models

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Todo
 
 def home(request):
     return render(request, "todo/home.html" )
+
+def todo_list(request):
+    todos = Todo.objects.all()    
